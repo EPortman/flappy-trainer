fe6d7a032a74f403caabaa7c9afef5db88c7774c
EPortman <ethan.portman@gov.bc.ca>
2024-11-08 21:54:38 -0800

    chore: add environment variables and update game manager to use them

--- flappy_trainer/game_manager.py
+++ flappy_trainer/game_manager.py
@@ -1,8 +1,15 @@
+import os
+
 import pygame
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class GameManager:
     def __init__(self):
-        self.SCREEN_WIDTH, self.SCREEN_HEIGHT = 400, 600
+        self.SCREEN_WIDTH = int(os.getenv("SCREEN_WIDTH", 800))
+        self.SCREEN_HEIGHT = int(os.getenv("SCREEN_HEIGHT", 600))
         self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Flappy Trainer")
