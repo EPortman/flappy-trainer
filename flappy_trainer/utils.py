79b6487bc4a5bf6352abb38c5e7458d093425ad4
EPortman <ethan.portman@gov.bc.ca>
2024-11-09 18:50:01 -0800

    feat: add enum for game state

--- flappy_trainer/utils.py
+++ flappy_trainer/utils.py
@@ -1,4 +1,12 @@
 import os
+from enum import Enum, auto
+
+
+class GameState(Enum):
+    START_MENU = auto()
+    RUNNING = auto()
+    PAUSED = auto()
+    GAME_OVER = auto()
 
 
 def get_env_var_as_int(var_name):
