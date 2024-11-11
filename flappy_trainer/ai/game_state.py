e60a49a0884c138a3a077a45735189e18745fe7b
EPortman <ethan.portman@gov.bc.ca>
2024-11-11 13:07:09 -0800

    feat: add class for game state

---
+++ flappy_trainer/ai/game_state.py
@@ -0,0 +1,16 @@
+class GameState:
+    def __init__(
+        self,
+        bird_vert_pos: int,
+        bird_vert_velocity: int,
+        distance_to_next_pipe: int,
+        next_pipe_top_open_pos: int,
+        next_pipe_bot_open_pos: int,
+        pipe_velocity: int,
+    ):
+        self.bird_vert_pos = bird_vert_pos
+        self.bird_vert_velocity = bird_vert_velocity
+        self.distance_to_next_pipe = distance_to_next_pipe
+        self.next_pipe_top_open_pos = next_pipe_top_open_pos
+        self.next_pipe_bot_open_pos = next_pipe_bot_open_pos
+        self.pipe_velocity = pipe_velocity
