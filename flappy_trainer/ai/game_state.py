b74ebc6435fa4571a40218e82feef1484ac303f0
EPortman <ethan.portman@gov.bc.ca>
2024-11-11 17:25:18 -0800

    feat: add method to get initial game state

--- flappy_trainer/ai/game_state.py
+++ flappy_trainer/ai/game_state.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from flappy_trainer.utils import get_env_var_as_int
+
 
 class GameState:
     def __init__(
@@ -28,4 +30,19 @@ class GameState:
                 self.next_pipe_bot_open_pos,
                 self.pipe_velocity,
             ]
+        )
+
+    @classmethod
+    def get_initial_game_state(cls) -> "GameState":
+        init_bird_vert_pos = get_env_var_as_int("BIRD_START_Y_POS")
+        init_bird_vert_velocity = get_env_var_as_int("BIRD_START_Y_VELOCITY")
+        init_pipe_speed = get_env_var_as_int("PIPE_SPEED") * (1 / 60)
+
+        return cls(
+            bird_vert_pos=init_bird_vert_pos,
+            bird_vert_velocity=init_bird_vert_velocity,
+            distance_to_next_pipe=None,
+            next_pipe_top_open_pos=None,
+            next_pipe_bot_open_pos=None,
+            pipe_velocity=init_pipe_speed,
         )
